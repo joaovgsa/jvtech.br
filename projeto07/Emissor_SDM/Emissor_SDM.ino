7398d9c34d056e3e100cb08758a3d74de284c36f
João Vitor Alvares <joaovgsa@gmail.com>
2022-11-02 08:46:14 -0300

    Add files via upload

---
+++ projeto07/Emissor_SDM/Emissor_SDM.ino
@@ -0,0 +1,51 @@
+//Bibliotecas 
+#include <SPI.h>
+#include <LoRa.h>
+
+//Definição dos pinos utilizados pelo LoRa
+#define SS 30
+#define RST 13
+#define D0 11
+
+void setup() {
+  //Inicia comunicação serial
+  Serial.begin(115200);
+
+  //Configura o LED e o sensor
+  pinMode(2, OUTPUT); //LED
+  pinMode(22, INPUT); //Sensor de Movimento
+
+  //Configura os pinos do Lora
+  LoRa.setPins(SS, RST, D0);
+  
+  //Configurando a frequência do LoRa
+  while (!LoRa.begin(915E6)) {
+    Serial.println(" ... ");
+    delay(500);
+  }
+  
+  //Sincroniza LoRa Emissor-Receptor
+  LoRa.setSyncWord(0xAA);
+  Serial.println("Sincronização Completa");
+}
+
+void loop() {
+
+    if (digitalRead(22)==1) //Se o sensor perceber movimento
+    {
+      Serial.print("Emitindo Mensagem: ");
+
+      //Liga o LED
+      digitalWrite(2, HIGH);
+
+      //Enviando a mensagem
+      LoRa.beginPacket();
+      LoRa.print("Movimento Detectado");
+      LoRa.endPacket();
+      delay(2000);
+
+      //Desliga o LED
+      digitalWrite(2, LOW);
+    }
+    delay(1000);
+}
---
+++ projeto08/Emissor_-_R/Emissor_-_R.ino
@@ -0,0 +1,72 @@
+//Bibliotecas 
+#include <SPI.h>
+#include <LoRa.h>
+
+//Definição dos pinos utilizados pelo LoRa
+#define SS 30
+#define RST 13
+#define D0 11
+
+int ciclo =  0;
+
+void setup() {
+  //Inicia comunicação serial
+  Serial.begin(115200);
+
+  //Configura o botão e o LED
+  pinMode(10, INPUT);  //Botão
+  pinMode(2, OUTPUT); //LED
+
+  //Configura os pinos do Lora
+  LoRa.setPins(SS, RST, D0);
+  
+  //Configurando a frequência do LoRa
+  while (!LoRa.begin(915E6)) {
+    Serial.println(" ... ");
+    delay(500);
+  }
+  
+  //Sincroniza LoRa Emissor-Receptor
+  LoRa.setSyncWord(0xAA);
+  Serial.println("Sincronização Completa");
+}
+
+void loop() {
+      //Placa é Emissor
+      Serial.print("Emitindo Mensagem: ");
+
+      //Pisca o LED
+      digitalWrite(2, HIGH);
+      delay(1000);
+      digitalWrite(2, LOW);
+      
+      //Enviando a mensagem
+      LoRa.beginPacket();
+      LoRa.print(ciclo);
+      LoRa.endPacket();
+      delay(2000);
+
+      //Placa é Receptor
+      int packetSize = LoRa.parsePacket();
+      if (packetSize)
+      {
+        //Uma mensagem foi recebida
+         Serial.print("Mensagem Recebida");
+           
+        //Pisca o LED
+        digitalWrite(2, HIGH);
+        delay(1000);
+        digitalWrite(2, LOW);
+  
+        //Leitura da mensagem
+        while (LoRa.available()) 
+        {
+            String LoRaData = LoRa.readString();
+            Serial.print(LoRaData); 
+        }
+        
+        ciclo ++; //Aumenta após recebimento da confirmação
+      }
+         
+        delay (2000);
+}
